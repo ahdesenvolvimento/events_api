13d77ea758f87fcd88c3209d6801772fa005f329
ahdev <53409839+ahdesenvolvimento@users.noreply.github.com>
2021-12-27 15:42:05 -0300

    retirando comentários

--- frontevents/src/components/pages/Event.js
+++ frontevents/src/components/pages/Event.js
@@ -72,8 +72,6 @@ export default function Event() {
       <p>{event.description}</p>
       <p>
         O evento ocorrerá no seguinte endereço <strong>{event.logradouro}, número {event.numero}, bairro {event.bairro} - {event.localidade}/{event.uf} - {event.cep} </strong>no dia {event.date_start} começando as {event.start_time} e terminando as {event.finish_time} do dia {event.date_finish}
-        {/* O evento começará as {event.start_time} com término as{" "}
-        {event.finish_time} do dia {event.date_start}  {event.finish_time && (<p>12312321</p>)} */}
       </p>
       <p>{event.total} participantes</p>
       {!statusEvent && (
