1168c9e2726f4d973b39181b52734d15412315c1
ahdev <53409839+ahdesenvolvimento@users.noreply.github.com>
2021-12-27 10:01:56 -0300

    melhorando o feedback para o usuário que deletar um evento, antes não retirava o evento da tela, agr já funcionando perfeitamente.

--- frontevents/src/components/layout/Event.js
+++ frontevents/src/components/layout/Event.js
@@ -1,33 +1,37 @@
 import { Link } from "react-router-dom";
 import styles from "./Event.module.css";
 import Card from "../layout/Card";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import MyEventsAuth from "./MyEventsAuth";
 export default function Event({ data, status }) {
-  // console.log(data)
-  // const [dados, setDados] = useState([data]);
+  const [dados, setDados] = useState([]);
+  const [controle, setControle] = useState(false);
 
-  // const controleDados = (e) =>{
-  //   setDados(dados);
-  //   data={};
-  //   console.log(dados)
-  // }
-  // useEffect(() => {
-  //   console.log(dados.length)
-  //   dados.length !== 0 ? controleDados() : setDados(data);
-  // });
+  const controleDados = (e) => {
+    setDados(dados);
+    setControle(true);
+  };
+  useEffect(() => {
+    if (dados.length !== 0) {
+      controleDados();
+    } else {
+      if (controle === false) {
+        setDados(data);
+      }
+    }
+  });
   function deleteEvent(id) {
     const init = {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("access-token")
+        Authorization: "Bearer " + localStorage.getItem("access-token"),
       },
     };
     fetch("http://localhost:8000/events/delete/" + id, init)
       .then((response) => response.json())
       .then((d) => {
-        // setDados(d);
+        setDados(d);
       })
       .catch((error) => console.log(error));
   }
@@ -35,18 +39,40 @@ export default function Event({ data, status }) {
     <>
       {status == 1 ? (
         <>
-          {data.length == 0 && <p className={styles.noEvents}>Sem eventos disponíveis</p>}
-          {data.map((event) => (
+          {dados.length == 0 && (
+            <p className={styles.noEvents}>Sem eventos disponíveis</p>
+          )}
+          {dados.map((event) => (
             <div className="col-md-4 mt-3" key={event.id}>
-              <Card title={event.title} styles={styles.minHeight} footer={<><Link to={"/event/" + event.id} className={styles.linkCustom}>
-                Saber mais
-              </Link></>} content={<><p className={styles.description}>{event.description}</p></>} />
+              <Card
+                title={event.title}
+                styles={styles.minHeight}
+                footer={
+                  <>
+                    <Link
+                      to={"/event/" + event.id}
+                      className={styles.linkCustom}
+                    >
+                      Saber mais
+                    </Link>
+                  </>
+                }
+                content={
+                  <>
+                    <p className={styles.description}>{event.description}</p>
+                  </>
+                }
+              />
             </div>
           ))}
         </>
       ) : (
         <>
-          <MyEventsAuth data={data} deleteEvent={deleteEvent} styles={styles} />
+          <MyEventsAuth
+            data={dados}
+            deleteEvent={deleteEvent}
+            styles={styles}
+          />
         </>
       )}
     </>
